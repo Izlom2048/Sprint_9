5431fb1d40812e786e69fa6e2a36dbeb6c4c8609
Mikhail Voronov <vbif542737@mail.ru>
2026-03-10 03:22:01 +0300

    Create branch

--- locators/recipe_page_locators.py
+++
@@ -1,7 +0,0 @@
-from selenium.webdriver.common.by import By
-
-
-class RecipePageLocators:
-    RECIPE_TITLE = (By.TAG_NAME, "h1")
-    DESCRIPTION_TITLE = (By.XPATH, "//h3[normalize-space()='Описание:']")
-    DESCRIPTION_TEXT = (By.XPATH, "//h3[normalize-space()='Описание:']/following-sibling::div")
